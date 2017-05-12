0c777a333139c5ca022e3da0c8e50f1d15c0a667
Juan C <jcpablo10@gmail.com>
2017-05-12 13:21:02 -0500

    Formulario de contacto listo

---
+++ php/contacto.php
@@ -0,0 +1,23 @@
+<?php
+	$nombre = $_POST['nombre'];
+	$mail = $_POST['mail'];
+	$telefono = $_POST['telefono'];
+	$descripcion = $_POST['descripcion'];
+	$para = "info@mimonolito.com";
+	$asunto ="Un cliente te ha contactado desde el sitio web.";
+	$header = "FROM: " . $mail;
+	$mensaje = "Nombre del cliente:" . $nombre . "\n E-Mail: " . $mail . " \n Telefono: " . $telefono . "\n Mensaje:\n " . $descripcion . "\n";
+
+	if (mail($para, $asunto, $mensaje, $header))
+	{
+		echo "<script language='javascript'>
+		alert('Mensaje enviado, muchas gracias.');
+		window.location.href = 'http://www.mimonolito.com';
+		</script>";
+	}
+	else
+	{
+		echo "Falló el envio, intente de nuevo o llámenos";
+	}
+
+?>
