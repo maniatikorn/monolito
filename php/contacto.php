7bbccdcf2e6ce1fac9b9c889c6384d4726bdd3a2
Juan C <jcpablo10@gmail.com>
2017-05-16 15:46:02 -0500

    Arreglos php

--- php/contacto.php
+++ php/contacto.php
@@ -4,11 +4,11 @@
 	$telefono = $_POST['telmovil'];
 	$descripcion = $_POST['descripcion'];
 	$para = "info@mimonolito.com";
-	$asunto ="Un cliente te ha contactado desde el sitio web.";
-	$header = "FROM: " . $mail;
-	$mensaje = "Nombre del cliente:" . $nombre . "\n E-Mail: " . $mail . " \n Telefono: " . $telefono . "\n Mensaje:\n " . $descripcion . "\n";
+	$asunto ="MONOLITO: Un cliente te ha contactado desde el sitio web.";
+	$headers = "FROM: " . $mail;
+	$mensaje = "Nombre del cliente: " . $nombre . "\n\n E-Mail: " . $mail . " \n\n Telefono: " . $telefono . "\n\n Mensaje:\n " . $descripcion . "\n\n";
 
-	if (mail($para, $asunto, $mensaje, $header))
+	if (mail($para, $asunto, $mensaje, $headers))
 	{
 		echo "<script language='javascript'>
 		alert('Mensaje enviado, muchas gracias.');
